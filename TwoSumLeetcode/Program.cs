194cba7448d1e83659743a781d767e89ffe8435e
baha mestiri <mohamedbaha.mestiri@gmail.com>
2022-11-17 14:52:49 +0100

    Add project files.

---
+++ TwoSumLeetcode/Program.cs
@@ -0,0 +1,10 @@
+﻿using TwoSumLeetcode; 
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Solution baha = new Solution();
+        baha.TwoSum(new int[] { 1, 2, 5,3 },4) ; 
+    }
+}
---
+++ TwoSumLeetcode/TwoSumProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSumLeetcode
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2] ;
+           ; int j = 1;
+            int x = nums.Length; 
+            while(j<x)
+            {
+                for ( int i=0 ; i<nums.Length; i++)
+                {
+                    Console.WriteLine(" nums de j " + nums[j]);
+                    Console.WriteLine("nums de i " + nums[i]);
+                    int y = nums[i] + nums[j]; Console.WriteLine(y);
+                    if (y == target) { Console.WriteLine("finally"); result[0] = i; result[1] = j; break;  }
+                }
+                j++;
+            }
+            Console.WriteLine(result[0] );
+            Console.WriteLine(result[1]);
+
+            return result; 
+        }
+    }
+}
