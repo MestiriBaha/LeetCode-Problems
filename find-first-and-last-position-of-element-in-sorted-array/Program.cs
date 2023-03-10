204bd06a790c68813d6cef9fe3ea49ce3bd7874f
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-03-10 19:18:02 +0100

    second Problem , Medium Level

---
+++ find-first-and-last-position-of-element-in-sorted-array/Program.cs
@@ -0,0 +1,78 @@
+﻿using LeetcodeProblem;
+public class Solution
+{
+     static int FirstOccurrence(int[] array, int n)
+    {
+
+        int beg = 0;
+        int end = array.Length - 1;
+
+        while (beg <= end)
+        {
+
+            int mid = beg + (end - beg) / 2;
+
+            if (array[mid] == n)
+            {
+                if (mid - 1 >= 0 && array[mid - 1] == n)
+                {
+                    end = mid - 1;
+                    continue;
+                }
+                return mid;
+            }
+
+            else if (array[mid] < n)
+                beg = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+     static int LastOccurrence(int[] array, int n)
+    {
+
+        int beg = 0;
+        int end = array.Length - 1;
+
+        while (beg <= end)
+        {
+
+            int mid = beg + (end - beg) / 2;
+
+            if (array[mid] == n)
+            {
+                if (mid + 1 < array.Length && array[mid + 1] == n)
+                {
+                    beg = mid + 1;
+                    continue;
+                }
+                return mid;
+
+            }
+
+            else if (array[mid] < n)
+                beg = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+    public static int[] SearchRange(int[] nums, int target)
+    {
+        if (nums.Length == 0)
+        {
+            return new int[] { -1, -1 };
+        }
+
+        List<int> result = new List<int>();
+        result.Add(FirstOccurrence(nums, target));
+        result.Add(LastOccurrence(nums, target));
+        return result.ToArray();
+
+    }
+
+
+}
+     
+   
---
+++ find-first-and-last-position-of-element-in-sorted-array/test1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblem
+{
+     public class test1
+    {
+        static void Main(string[] args)
+        {
+            foreach (int term in Solution.SearchRange(new int[] { 1, 2, 3, 4, 4, 7, 7, 8 }, 4))
+            {
+                Console.WriteLine(term);
+            }
+        }
+    }
+}
